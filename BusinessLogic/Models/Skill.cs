eb735edb4154800ff8242b39703ea666a13e66ef
Bartosz Wójcik <bartosz.wojcik@si-consulting.pl>
2019-12-01 18:55:15 +0100

    Initial commit

---
+++ BusinessLogic/Models/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BusinessLogic.Models
+{
+	public class Skill
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class HeroSkills
+	{
+		public int Id { get; set; }
+		public virtual Skill Skill { get; set; }
+		public int SkillId { get; set; }
+		public virtual Hero Hero { get; set; }
+		public Guid HeroId { get; set; }
+		public int Points { get; set; }
+	}
+}
---
+++ DataAccess/Configuration/MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Configuration
+{
+	public partial class MyDbContext : DbContext
+	{
+		public MyDbContext()
+		{
+
+		}
+
+		public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+		{
+
+		}
+
+
+	}
+}
